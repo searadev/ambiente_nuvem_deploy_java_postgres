0758ba5a07d13cadede7d51203d7ca95d14682e6
searadev <searadejesusdev@gmail.com>
2022-09-27 10:00:11 -0300

    include title in psy, message, and add data.sql

--- create_tables.sql
+++ create_tables.sql
@@ -6,8 +6,8 @@ create table tb_medium (id bigserial not null, full_name varchar(255), primary k
 create table tb_module (id bigserial not null, name varchar(255), primary key (id));
 create table tb_class (id bigserial not null, title varchar(255), url varchar(255), date date, created_at TIMESTAMP WITHOUT TIME ZONE, updated_at TIMESTAMP WITHOUT TIME ZONE, module_id int8 not null, medium_id int8 not null, user_id int8 not null, primary key (id));
 create table tb_lecture (id bigserial not null, title varchar(255), url varchar(255), date date, created_at TIMESTAMP WITHOUT TIME ZONE, updated_at TIMESTAMP WITHOUT TIME ZONE, medium_id int8 not null, user_id int8 not null, primary key (id));
-create table tb_message (id bigserial not null, full_name varchar(255), text text, date date, created_at TIMESTAMP WITHOUT TIME ZONE, updated_at TIMESTAMP WITHOUT TIME ZONE, medium_id int8 not null, user_id int8 not null, status boolean not null, primary key (id));
-create table tb_psychography (id bigserial not null, full_name varchar(255), text text, date date, created_at TIMESTAMP WITHOUT TIME ZONE, updated_at TIMESTAMP WITHOUT TIME ZONE, medium_id int8 not null, user_id int8 not null, status boolean not null, primary key (id));
+create table tb_message (id bigserial not null, full_name varchar(255), title varchar(255),text text, date date, created_at TIMESTAMP WITHOUT TIME ZONE, updated_at TIMESTAMP WITHOUT TIME ZONE, medium_id int8 not null, user_id int8 not null, status boolean not null, primary key (id));
+create table tb_psychography (id bigserial not null, full_name varchar(255),title varchar(255), text text, date date, created_at TIMESTAMP WITHOUT TIME ZONE, updated_at TIMESTAMP WITHOUT TIME ZONE, medium_id int8 not null, user_id int8 not null, status boolean not null, primary key (id));
 alter table tb_user add constraint UK_4vih17mube9j7cqyjlfbcrk4m unique (email);
 alter table tb_user_role add constraint FKea2ootw6b6bb0xt3ptl28bymv foreign key (role_id) references tb_role;
 alter table tb_user_role add constraint FK7vn3h53d0tqdimm8cp45gc0kl foreign key (user_id) references tb_user;
